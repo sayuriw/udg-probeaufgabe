96db232429eeb44f816eb679ab89445e7151fd48
sayuriw <sayuri.wichmann@gmail.com>
2019-12-03 15:26:06 +0100

    filter logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,27 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useContext } from 'react'
 import { ItemsContext } from '../provider'
 import styled from 'styled-components/macro'
-import Item from './Item'
+import ItemsPage from './ItemsPage'
 import CreateItem from './CreateItem'
-import { getItems, postItem, deleteItem, patchItem } from './services'
+import { postItem } from './services'
 
 export default function App() {
   const [items, setItems] = useContext(ItemsContext)
-
-  useEffect(() => {
-    filterItems()
-  }, [])
-
+  
   return (
     <PageStyled>
       <HeaderStyled>List of Items</HeaderStyled>
       <CreateItem onSubmit={createItem}/>
-      {items.map(item => (
-        <Item
-          key={item._id}
-          onDeleteClick={() => handleDeleteClick(item)}
-          onSubmit={handleEditClick}
-          {...item}
-        />
-      ))}
+      <ItemsPage />
     </PageStyled>
   )
 
-  function filterItems() {
-    getItems(items).then(items => {
-      const filteredItems = items.filter(
-        item => item.Produktart === 'T-Shirts'
-      )
-      setItems(filteredItems)
-    })
-  }
   function createItem(itemData) {
     postItem(itemData).then(item => {
       setItems([item, ...items])
     })
   }
-   function handleEditClick(editData) {
-     const id = editData._id
-     patchItem(id, editData).then(editItem => {
-     const index = items.findIndex(item => item._id === id)
-       setItems([...items.slice(0, index), editItem, ...items.slice(index + 1)])
-     })
-   }
-  function handleDeleteClick(item) {
-    if (window.confirm('Are you sure you wish to delete this item?')) {
-      deleteItem(item._id).then(deletedItem => {
-        const index = items.findIndex(item => item._id === deletedItem._id)
-        setItems([...items.slice(0, index), ...items.slice(index + 1)])
-      })
-    }
-  }
+  
 }
 
 const PageStyled = styled.main`
---
+++ src/components/Filter.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+export default function Filter({ onClick, filters, selectedFilter }) {
+  return (
+    <Grid>
+      <ButtonStyled
+        active={selectedFilter === 'all'}
+        onClick={() => onClick('all')}>
+        all
+      </ButtonStyled>
+      {filters.map(filter => (
+        <ButtonStyled
+          active={selectedFilter === filter}
+          onClick={handleFilterClick}
+          key={filter}>
+          {filter}
+        </ButtonStyled>
+      ))}
+    </Grid>
+  )
+
+  function handleFilterClick(event) {
+    const filter = event.target.textContent
+    onClick(filter)
+  }
+}
+
+const Grid = styled.section`
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+  scroll-snap-type: x mandatory;
+  margin-top: 1px;
+  display: grid;
+  grid-auto-flow: column;
+  gap: 1px;
+  width: 100%;
+`
+
+function select(propName, first, second) {
+  return props => (props[propName] ? first : second)
+}
+
+const ButtonStyled = styled.button`
+  background: ${select('active', 'hotpink', '#444')};
+  color: ${select('active', 'white', 'white')};
+  border: none;
+  scroll-snap-align: start;
+`
